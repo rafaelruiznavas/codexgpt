6a2f62d388872130b1d86a3e440e47063fe29d3c
Rafa Ruiz <rafa@ruiznavas.com>
2023-01-02 11:37:34 +0100

    Change server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // Fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://rafacodex.onrender.com', {
     method: 'POST',
     headers:{
       'Content-Type': 'application/json'
